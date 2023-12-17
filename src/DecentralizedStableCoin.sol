0373810a5af2895e7ef33fb04dc0d5155cd5bd11
Sean Coates <sean.coates+fooyay@gmail.com>
2023-12-17 13:51:56 -0500

    start README and begin coding

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of contract:
+// version
+// imports
+// errors
+// interfaces, libraries, and contracts
+// type declarations
+// state variables
+// events
+// modifiers
+// functions
+
+// Layout of functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure
+
+pragma solidity ^0.8.18;
+
+contract DecentralizedStableCoin {}
