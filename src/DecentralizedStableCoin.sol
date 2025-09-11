8c46dd59b9befcc5aec15c4474e2e6debd8d4c10
Sean Coates <sean.coates+fooyay@gmail.com>
2025-09-11 15:39:19 -0400

    add comment

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -21,7 +21,7 @@
 // private
 // view & pure
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -44,13 +44,11 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
 
     address initial_owner = msg.sender; // TODO: change this to actual owner
 
-    constructor()
-        ERC20("DecentralizedStableCoin", "DSC")
-        Ownable(initial_owner)
-    {}
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
+        // technically, we don't need to check for <0 here, because uint256 can't be <0
         if (_amount <= 0) {
             revert DecentralizedStableCoin__MustBeMoreThanZero();
         }
@@ -60,10 +58,7 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
         super.burn(_amount);
     }
 
-    function mint(
-        address _to,
-        uint256 _amount
-    ) external onlyOwner returns (bool) {
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
         if (_to == address(0)) {
             revert DecentralizedStableCoin__NotZeroAddress();
         }
